6b91f4d1013b60a7611d5a793868a0a19721e64c
Nik <Nikhilkurrey135@gmail.com>
2024-12-24 22:01:28 +0530

    login session implemented

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,49 @@
 import { View, Text } from 'react-native';
-import React from 'react';
-import { Stack } from 'expo-router';
+import React, { useEffect } from 'react';
+import { Stack ,useRouter} from 'expo-router';
+import { AuthProvider, useAuth } from '../context/AuthContext';
+import { supabase } from '../lib/supabase';
 
-const _layout = () => {
+
+
+const _layout = ()=>
+{
+  return (
+    <AuthProvider>
+      <MainLayout/>
+    </AuthProvider>
+  )
+}
+const MainLayout = () => {
+  const {setAuth} = useAuth();
+  const router = useRouter();
+
+  useEffect(()=>{
+
+// bellow is the auth listern it trigger every time when user login , register,logsout
+    supabase.auth.onAuthStateChange((_event, session)=>
+    {
+      console.log('session user: ',session?.user?.id);
+      // uper walle code se id and user ka (?) hata doge to purrra session detial (user meta deta mill jayga    ) 
+
+      // setSession(session);
+      if(session)//agar session ture h yani user login h tho home me jane bol sakte ha 
+      {
+        setAuth(session?.user);
+        router.replace('main/home');// we willl replace current route so u user cannot go back to welcome page again 
+        //first set auth is user ke liye 
+        // move to home screen f
+      }
+      else{
+        setAuth(null);
+        router.replace('/welcome');
+        //set auth to null 
+        // move to welcom screen 
+
+      };
+    });
+    // return () => unsubscribe(); 
+  },[])//need to add this empty array very neccessary , for dependences , warna glitch hoga 
   return (
     <Stack
       screenOptions={{
--- app/index.jsx
+++ app/index.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useRouter } from 'expo-router';
 import ScreenWrapper from '../components/ScreenWrapper'; // Assuming it's in the 'components' folder relative to the current file
+import Loading from '../components/Loading';
 
 
 const Index = () => {
   const router = useRouter();
   
   return (
-    <ScreenWrapper>
-      <Text>Index</Text>
-      {/* Correct Button Implementation */}
-      <Button title="welcome😉" onPress={() => router.push('welcome')} />
-    </ScreenWrapper>
+    // no need , cause now we gonna check ki user ka session key active h y ni us hisab se usko home ya login page me rakhenge ge 
+    // belwo comment code is veryfirst page off aap(index and , wellcome button wala)
+    // <ScreenWrapper>
+    //   <Text>Index</Text>
+    //   {/* Correct Button Implementation */}
+    //   <Button title="welcome😉" onPress={() => router.push('welcome')} />
+    // </ScreenWrapper>
+
+    //! checking for sesssion from supabase, do loading state after fetching , redirect to home
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+  {/* Display a loading message while the component is loading */}
+    <Loading/>
+    </View>
   );
 };
 
